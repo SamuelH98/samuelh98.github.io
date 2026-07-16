7f07b4d42497b8acbd768ed74962f6cc6169d19a
Samuel Hale <samworlds1231337@gmail.com>
2026-07-15 23:38:30 -0400

    Fix Portfolio link URL in resume tailor template

--- public/js/resume-tailor.js
+++ public/js/resume-tailor.js
@@ -241,7 +241,7 @@
     ${tEsc(header.phone)} #h(0.35em)|#h(0.35em)
     #link("mailto:${header.email}")[${tEsc(header.email)}] #h(0.35em)|#h(0.35em)
     #link("https://${header.linkedin}")[${tEsc(header.linkedin)}] #h(0.35em)|#h(0.35em)
-    #link("https://${header.site}")[${tEsc(header.site)}] #h(0.35em)|#h(0.35em)
+    #link("https://samuelh98.github.io")[${tEsc(header.site)}] #h(0.35em)|#h(0.35em)
     ${tEsc(header.citizenship)}
   ]
 ]
